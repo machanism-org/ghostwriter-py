f14757943809f654b6c5476da2b1fad80fb4cb8a
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-09-25 13:02:56 +0300

    update Python Ghostwriter wrapper

--- machai.gw/machai/gw/ghostwriter.py
+++ machai.gw/machai/gw/ghostwriter.py
@@ -6,22 +6,17 @@ import jpype.imports
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 JAR_PATH = os.path.join(BASE_DIR, "jars", "ghostwriter.jar")
 
-def ensure_jvm():
+def gw(args: list[str]) -> str:
     if not jpype.isJVMStarted():
         jpype.startJVM(
             jpype.getDefaultJVMPath(),
             f"-Djava.class.path={JAR_PATH}",
             convertStrings=True
         )
-
-def gw(args: list[str]) -> str:
-    ensure_jvm()
     
     from org.machanism.machai.gw.processor import Ghostwriter
     
     result = Ghostwriter.main(args)
-    print(result)
-    
     return result
     
 if __name__ == "__main__":
